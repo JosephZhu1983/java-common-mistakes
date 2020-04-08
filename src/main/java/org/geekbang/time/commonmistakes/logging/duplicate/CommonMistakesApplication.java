b9d26c358df42b538bf69d64a6fe4d1aa720d2c6
朱晔 <ye.zhu001@BKJK.COM>
2020-04-08 10:46:17 +0800

    更新MultipleLevelsFilter的例子

--- src/main/java/org/geekbang/time/commonmistakes/logging/duplicate/CommonMistakesApplication.java
+++ src/main/java/org/geekbang/time/commonmistakes/logging/duplicate/CommonMistakesApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CommonMistakesApplication {
 
     public static void main(String[] args) {
-        System.setProperty("logging.config", "classpath:org/geekbang/time/commonmistakes/logging/duplicate/filterright.xml");
+        System.setProperty("logging.config", "classpath:org/geekbang/time/commonmistakes/logging/duplicate/multiplelevelsfilter.xml");
         SpringApplication.run(CommonMistakesApplication.class, args);
     }
 }
