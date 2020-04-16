af931d27a5132edac90cf220ec841584585fe781
朱晔 <ye.zhu001@BKJK.COM>
2020-04-16 13:11:28 +0800

    补充Optional的测试

--- src/main/java/org/geekbang/time/commonmistakes/nullvalue/pojonull/POJONullController.java
+++ src/main/java/org/geekbang/time/commonmistakes/nullvalue/pojonull/POJONullController.java
@@ -1,11 +1,11 @@
 package org.geekbang.time.commonmistakes.nullvalue.pojonull;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RequestMapping("pojonull")
@@ -17,6 +17,17 @@ public class POJONullController {
 
     @Autowired
     private UserRepository userRepository;
+
+    @GetMapping
+    public void test() throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new Jdk8Module());
+        UserDto result = objectMapper.readValue("{\"id\":\"1\", \"age\":30, \"name\":null}", UserDto.class);
+        log.info("field name with null value dto:{} name:{}", result, result.getName().orElse("N/A"));
+        //field name with null value dto:UserDto(id=1, name=Optional.empty, age=Optional[30]) name:N/A
+        log.info("missing field name dto:{}", objectMapper.readValue("{\"id\":\"1\", \"age\":30}", UserDto.class));
+        // missing field name dto:UserDto(id=1, name=null, age=Optional[30])
+    }
 
     @PostMapping("wrong")
     public User wrong(@RequestBody User user) {
