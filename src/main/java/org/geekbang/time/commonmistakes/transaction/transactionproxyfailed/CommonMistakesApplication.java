2ced285a21450993df51b71d840cf29c7cd3b95f
朱晔 <ye.zhu001@BKJK.COM>
2020-06-11 18:14:55 +0800

    AspectJ配置AOP的测试

--- src/main/java/org/geekbang/time/commonmistakes/transaction/transactionproxyfailed/CommonMistakesApplication.java
+++ src/main/java/org/geekbang/time/commonmistakes/transaction/transactionproxyfailed/CommonMistakesApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
+//@EnableTransactionManagement(mode = AdviceMode.ASPECTJ)
 public class CommonMistakesApplication {
 
     public static void main(String[] args) {
