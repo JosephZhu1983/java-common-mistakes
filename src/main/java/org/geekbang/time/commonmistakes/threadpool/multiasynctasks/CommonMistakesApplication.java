51bc6312de1097112bca158a5937795b0c8cb353
朱晔 <ye.zhu1@viatris.com>
2023-04-22 15:30:02 +0800

    补充

--- src/main/java/org/geekbang/time/commonmistakes/threadpool/multiasynctasks/CommonMistakesApplication.java
+++ src/main/java/org/geekbang/time/commonmistakes/threadpool/multiasynctasks/CommonMistakesApplication.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.*;
+import java.util.function.Supplier;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -13,8 +14,8 @@ public class CommonMistakesApplication {
 
     private static ExecutorService threadPool = Executors.newFixedThreadPool(10);
 
-    public static void main(String[] args) {
-        test2();
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        test3();
     }
 
     private static void test1() {
@@ -66,5 +67,35 @@ public class CommonMistakesApplication {
             countDownLatch.countDown();
         };
     }
+
+    private static void test3() throws ExecutionException, InterruptedException {
+        long begin = System.currentTimeMillis();
+        List<Integer> result = new ArrayList<>();
+        List<CompletableFuture<Integer>> futures = IntStream.rangeClosed(1, 4)
+                .mapToObj(i -> CompletableFuture.
+                        supplyAsync(getAsyncTaskSupplier(i))
+                        .whenComplete((r, t) -> result.add(r)))
+                .collect(Collectors.toList());
+
+        try {
+            CompletableFuture.allOf(futures.toArray(new CompletableFuture[0]))
+                    .get(3, TimeUnit.SECONDS);
+        } catch (TimeoutException ignore) {
+        }
+
+        log.info("result {} took {} ms", result, System.currentTimeMillis() - begin);
+    }
+
+    private static Supplier<Integer> getAsyncTaskSupplier(int i) {
+        return () -> {
+            try {
+                TimeUnit.SECONDS.sleep(i);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            return i;
+        };
+    }
+
 }
 
